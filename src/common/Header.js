506900c7311ce9b79414dddaff4ba1947aa12606
andrevmog <andre.machado@zeeng.com.br>
2021-10-05 01:08:26 -0300

    home page

---
+++ src/common/Header.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import React from 'react';
+import '../css/Header.css';
+
+export default ({black}) => {
+ 
+    return(
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix"></img>
+                </a>
+            </div>
+            <div className="header--userSettings">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="user"></img>
+                </a>
+            </div>
+        </header>
+    );
+}
